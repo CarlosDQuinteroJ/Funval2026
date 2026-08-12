8e7c8e723911f98fc911a8176ff55e604f08fab0
GochoVnz <carlosdavid.chicharito@gmail.com>
2026-08-11 23:33:35 -0500

    feat: menú interactivo con ciclo y manejo de arreglo

---
+++ index2.ts
@@ -0,0 +1,40 @@
+import { createInterface } from "readline/promises";
+import { stdin as input, stdout as output } from "process";
+
+const rl = createInterface({ input, output });
+
+const tareas: string[] = [];
+
+let opcion = "";
+
+while (opcion !== "4") {
+
+    console.log("===== GESTOR DE TAREAS =====");
+    console.log("1. Agregar tarea");
+    console.log("2. Eliminar última tarea");
+    console.log("3. Listar tareas");
+    console.log("4. Salir");
+
+    opcion = await rl.question("Elige una opción: ");
+
+    if (opcion === "1") {
+
+        const tarea = await rl.question("Escribe una tarea: ");
+
+        tareas.push(tarea);
+
+        console.log("Tarea agregada");
+
+    } else if (opcion === "3") {
+
+        for (let i = 0; i < tareas.length; i++) {
+
+            console.log(`${i + 1}. ${tareas[i]}`);
+
+        }
+
+    }
+
+}
+
+console.log("Programa terminado");
