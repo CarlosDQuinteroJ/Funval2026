b71f5c9c8afbf837d2f07d20c3936728ab941c6f
GochoVnz <carlosdavid.chicharito@gmail.com>
2026-08-12 23:39:09 -0500

    Subiendo solo el archivo index3.ts

---
+++ index3.ts
@@ -0,0 +1,93 @@
+import { createInterface } from "readline/promises";
+import { stdin as input, stdout as output } from "process";
+
+const rl = createInterface({ input, output });
+
+interface Task {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const tareas: Task[] = [];
+
+let id = 1;
+
+const addTask = (title: string) => {
+
+    const tarea: Task = {
+        id: id,
+        title: title,
+        completed: false
+    };
+
+    tareas.push(tarea);
+    id++;
+
+    console.log("Tarea agregada");
+};
+
+const listTasks = () => {
+
+    for (let i = 0; i < tareas.length; i++) {
+
+        let estado = "pending";
+
+        if (tareas[i].completed === true) {
+            estado = "completed";
+        }
+
+        console.log(
+            `[${tareas[i].id}] ${tareas[i].title} - ${estado}`
+        );
+    }
+};
+
+const removeTask = () => {
+
+    const tareaEliminada = tareas.pop();
+
+    if (tareaEliminada) {
+        console.log(`Tarea eliminada: ${tareaEliminada.title}`);
+    } else {
+        console.log("No hay tareas para eliminar");
+    }
+};
+
+let opcion = "";
+
+while (opcion !== "4") {
+
+    console.log("===== GESTOR DE TAREAS =====");
+    console.log("1. Agregar tarea");
+    console.log("2. Eliminar última tarea");
+    console.log("3. Listar tareas");
+    console.log("4. Salir");
+
+    opcion = await rl.question("Elige una opción: ");
+
+    if (opcion === "1") {
+
+        const title = await rl.question("Escribe una tarea: ");
+
+        addTask(title);
+
+    } else if (opcion === "2") {
+
+        removeTask();
+
+    } else if (opcion === "3") {
+
+        listTasks();
+
+    } else if (opcion === "4") {
+
+        console.log("Programa terminado");
+
+    } else {
+
+        console.log("Opción incorrecta");
+    }
+}
+
+rl.close();
